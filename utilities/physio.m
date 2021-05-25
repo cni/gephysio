05603903937c21f694a78fec7639b2c4f8917ac8
Brian Wandell <wandell@stanford.edu>
2021-05-25 15:15:52 -0700

    added data and updated physio

--- utilities/physio.m
+++ utilities/physio.m
@@ -1,35 +1,68 @@
+%% We downloaded some PPG data from
+%
+%  From the iris project, part of the LEANEW1 group
+%  resting_2 physio data
+%
+
+%% Here are the scitran commands for finding and downloading the data
+
+% Brian to complete
+% st = scitran('cni');
+%
+
+%%
+chdir(fullfile(gpRootPath,'data','gephysio1'));
+
+% These are the pulsimetry values measured every 10 ms
 foo = readmatrix('PPGData_cni_epi_0220202113_25_28_493');
 
 ppg = foo(:,2);
 
+% We convert from 10ms to 1 ms values
 t = ((1:numel(ppg))-1)*10;
-
-% ieNewGraphWin;
 
 figure; 
 
-r = 9000:10000;
+% Choose a range of times to plot
+start = 10000;
+stop  = 30000;
+r = logical(start < t) & logical(t < stop);
 
 plot(t(r),ppg(r));
+xlabel('Time (ms)');
+ylabel('PPG value');
+
+%% These are times that the PPG detects as an event
 
 trig = readmatrix('PPGTrig_cni_epi_0220202113_25_28_493');
 
-lst = (trig > r(1)) & (trig < r(end));
+% Trigger in milliseconds
+trig = trig*10;
 
-val = trig(lst);
+lst = logical(start < trig) & logical (trig < stop);
+
+% lst = (trig > r(1)) & (trig < r(end));
+
+% Convert to milliseconds
+% val = trig(lst)*10;
 
 hold on;
+mx = max(ppg(r)); mn = min(ppg(r));
+% for ii=1:numel(lst)
+%     line([trig(lst),trig(lst)],[mn,mx],'Color','k');
+% end
 
-plot(val*10,ones(size(val))*100,'ko');
+set(gca,'ylim',[mn mx]);
+plot(trig(lst),ones(size(trig(lst)))*mx*0.9,'ko');
 
-%%
+%%  Respiration data
+
 foo = readmatrix('RESPData_cni_epi_0220202113_25_28_493');
 
 resp = foo(:,2);
 
+% The samples were at 40 ms, not 10 ms
 t = ((1:numel(resp))-1)*40;
-
-% ieNewGraphWin;
 
 figure; 
 
@@ -41,9 +74,12 @@ trig_resp = readmatrix('RESPTrig_cni_epi_0220202113_25_28_493');
 
 lst = (trig_resp > r(1)) & (trig_resp < r(end));
 
-val = trig_resp(lst);
+val = trig_resp(lst)*40;
 
 hold on;
 
-plot(val*10,ones(size(val))*3000,'ko');
+plot(val,ones(size(val))*3000,'ko');
 
+%% Write out simple summary of the 
+
+%% END
